569a43d297e97f4aa7cb7727e589da6350f41154
Yihang Ho <hoyihang5@gmail.com>
2017-05-11 12:14:19 +0800

    Move default sender to config

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'bla@yihangho.com'
+  default from: ENV['DEFAULT_SENDER']
 
   def welcome_email(email)
     mail(to: email, subject: "Welcome!")
