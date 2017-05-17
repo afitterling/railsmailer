3772b44081a86fbafcbc2e4cc7decc20518f26fa
Yihang Ho <hoyihang5@gmail.com>
2017-05-17 22:18:56 +0800

    Generate device model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211062115) do
+ActiveRecord::Schema.define(version: 20170517141718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20170211062115) do
   end
 
   add_index "access_tokens", ["user_id"], name: "index_access_tokens_on_user_id", using: :btree
+
+  create_table "devices", force: :cascade do |t|
+    t.integer  "last_used_key_sequence", default: -1, null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",              default: "", null: false
